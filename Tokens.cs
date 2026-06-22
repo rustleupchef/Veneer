9cb3def1ebc922d92c57d68096008c48192fc6b6
rustleupchef <rustle.up.chef@gmail.com>
2026-06-22 13:45:02 -0500

    added Tokens.cs File; included most notable tokens for this program

---
+++ Tokens.cs
@@ -0,0 +1,30 @@
+namespace Veneer;
+
+public class Tokens
+{
+    public enum TokenType 
+    {
+        // Special
+        EndOfFile, Invalid,
+
+        // Identifiers & Literals
+        Identifier, IntLiteral, FloatLiteral, StringLiteral, BoolLiteral,
+
+        // Keywords
+        For, While, If, Else, Return,
+        Int, Float, Bool, Char, Void, String,
+        Tooth, Language,
+
+        // Operators
+        Plus, Minus, Star, Slash, Modulo,
+        Assign, EqualEqual, NotEqual,
+        LessThan, LessEqual, GreaterThan, GreaterEqual,
+        AmpAmp, PipePipe, Bang,
+
+        // Punctuation
+        LeftParen, RightParen, LeftBrace, RightBrace, LeftBracket, RightBracket,
+        Comma, SemiColon
+    }
+    
+    public record Token(TokenType Type, string Value);
+}
