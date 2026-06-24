420e5b70e3b7a2778899e9feeb925a027da7927b
rustleupchef <rustle.up.chef@gmail.com>
2026-06-24 13:12:57 -0500

    added rudimentary transpiler in Transpiler.cs; TODO: implement tooth system

--- Program.cs
+++ Program.cs
@@ -6,9 +6,16 @@ internal class Program
     {
         List<Tokens.Token> tokens = Lexer.LexText(File.ReadAllText(Console.ReadLine()));
 
-        foreach (Tokens.Token token in tokens)
+        try
         {
-            Console.WriteLine(token);
+            Transpiler transpiler = new Transpiler(tokens);
+            string result = transpiler.Transpile();
+            File.WriteAllText(Console.ReadLine(), result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
         }
     }
 }
